a19d897110ba31ce37f3ec76b3bec827e6debd63
AnkitShome <ankitshome7@gmail.com>
2024-09-30 00:42:33 +0530

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from "../components/Hero"
+import LatestCollection from "../context/LatestCollection"
+
+
+const Home = () => {
+   return (
+      <div>
+         <Hero />
+         <LatestCollection />
+      </div>
+   )
+}
+
+export default Home
