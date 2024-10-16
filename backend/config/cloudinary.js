302e9eb4d554fa5803077e2a948de9a298a0ec29
AnkitShome <ankitshome7@gmail.com>
2024-10-16 19:53:10 +0530

    api for user and products created admin control added

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,11 +1,15 @@
-import { v2 as cloudinary } from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from 'dotenv'; // Ensure dotenv is imported to load environment variables
+
+dotenv.config(); // Load environment variables
 
 const connectCloudinary = async () => {
    cloudinary.config({
       cloud_name: process.env.CLOUDINARY_NAME,
       api_key: process.env.CLOUDINARY_API_KEY,
       api_secret: process.env.CLOUDINARY_SECRET_KEY
-   })
-}
+   });
+
+};
 
 export default connectCloudinary;
